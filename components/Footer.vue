a092828c023dc52721212dc1a72e29fc70fa2026
kykt35 <kykt35@gmail.com>
2021-02-24 15:39:43 +0900

    Create Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="root">
+    <div class="copyright">© 2018 Copyright Text</div>
+  </footer>
+</template>
+
+<style scoped>
+.root {
+  height: 40px;
+  background-color: #607d8b;
+}
+
+.copyright {
+  color: #fff;
+  font-size: 16px;
+  width: 200px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
