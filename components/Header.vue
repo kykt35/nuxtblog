7f5536812e137325f81113454f6acd6dda544773
kykt35 <kykt35@gmail.com>
2021-02-24 15:39:31 +0900

    Create Header component

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="root">
+    <div class="logo">NuxtBlog</div>
+  </header>
+</template>
+
+<style scoped>
+.root {
+  height: 80px;
+  background-color: #607d8b;
+}
+
+.logo {
+  color: #fff;
+  font-size: 32px;
+  width: 200px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
