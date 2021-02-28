82800b13980014dab5da51c7d866b9b86a580449
kykt35 <kykt35@gmail.com>
2021-02-28 18:30:29 +0900

    Create article page

---
+++ pages/articles/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="article_box">
+    <div class="title">{{ title }}</div>
+    <div class="description">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article_page',
+
+  props: {},
+  data() {
+    return {
+      title: 'タイトル',
+      description:
+        'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.article_box {
+  margin: 20px;
+}
+.title {
+  font-size: 28px;
+  margin-bottom: 30px;
+}
+.description {
+  font-size: 12px;
+}
+</style>
