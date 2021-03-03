e056f3afcd0679f85502b32328b3bc06e3c79237
kykt35 <kykt35@gmail.com>
2021-03-03 15:51:20 +0900

    Update article page for useing vuex

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="article_box">
     <div class="title">記事一覧</div>
-    <nuxt-link to="articles/new">新規投稿</nuxt-link>
-    <div v-for="article in articles" :key="article.id" class="article_card">
-      <nuxt-link :to="'articles/' + article.id">{{ article.title }}</nuxt-link>
+    <div class="button_wrap">
+      <button @click="onClickButton">新規投稿</button>
     </div>
+    <div v-if="articles.length">
+      <div v-for="article in articles" :key="article.id" class="article_card">
+        <nuxt-link :to="'articles/' + article.id">{{
+          article.title
+        }}</nuxt-link>
+      </div>
+    </div>
+    <div v-else>記事がありません</div>
   </div>
 </template>
 
 <script>
-import { articles } from '../../lib/articles_data'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'ArticlesPndexPage',
-  asyncData() {
-    return { articles }
+  computed: {
+    ...mapGetters(['articles']),
+  },
+  methods: {
+    onClickButton(e) {
+      e.preventDefault()
+      this.$router.push('/articles/new')
+    },
   },
 }
 </script>
@@ -27,7 +40,8 @@ export default {
   font-size: 28px;
   margin-bottom: 30px;
 }
-.description {
-  font-size: 12px;
+
+.button_wrap {
+  margin-bottom: 20px;
 }
 </style>
